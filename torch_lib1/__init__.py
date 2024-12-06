ce721feb66ca7c807b5e6eda02a9464a8dd90a83
yusei603 <ap_2970002193872@outlook.jp>
2024-12-06 16:38:35 +0900

    Update __init__.py

--- torch_lib1/__init__.py
+++ torch_lib1/__init__.py
@@ -3,4 +3,220 @@
 # https://www.apache.org/licenses/LICENSE-2.0.html
 
 # ReadMe
-README = 
+README = 'Private Library for PyTorch\nAuthor: K. Yusei'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+import torch
+from torch import tensor
+import torch.nn as nn
+import torch.optim as optim
+from torchviz import make_dot
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as datasets
+from tqdm.notebook import tqdm
+
+# 損失関数値計算用
+def eval_loss(loader, device, net, criterion):
+  
+    # DataLoaderから最初の1セットを取得する
+    for images, labels in loader:
+        break
+
+    # デバイスの割り当て
+    inputs = images.to(device)
+    labels = labels.to(device)
+
+    # 予測値の計算
+    outputs = net(inputs)
+
+    #  損失値の計算
+    loss = criterion(outputs, labels)
+
+    return loss
+  
+
+# 学習用関数
+def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history):
+
+    base_epochs = len(history)
+  
+    for epoch in range(base_epochs, num_epochs+base_epochs):
+        # 1エポックあたりの正解数(精度計算用)
+        n_train_acc, n_val_acc = 0, 0
+        # 1エポックあたりの累積損失(平均化前)
+        train_loss, val_loss = 0, 0
+        # 1エポックあたりのデータ累積件数
+        n_train, n_test = 0, 0
+
+        #訓練フェーズ
+        net.train()
+
+        for inputs, labels in tqdm(train_loader):
+            # 1バッチあたりのデータ件数
+            train_batch_size = len(labels)
+            # 1エポックあたりのデータ累積件数
+            n_train += train_batch_size
+    
+            # GPUヘ転送
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # 勾配の初期化
+            optimizer.zero_grad()
+
+            # 予測計算
+            outputs = net(inputs)
+
+            # 損失計算
+            loss = criterion(outputs, labels)
+
+            # 勾配計算
+            loss.backward()
+
+            # パラメータ修正
+            optimizer.step()
+
+            # 予測ラベル導出
+            predicted = torch.max(outputs, 1)[1]
+
+            # 平均前の損失と正解数の計算
+            # lossは平均計算が行われているので平均前の損失に戻して加算
+            train_loss += loss.item() * train_batch_size 
+            n_train_acc += (predicted == labels).sum().item() 
+
+        #予測フェーズ
+        net.eval()
+
+        for inputs_test, labels_test in test_loader:
+            # 1バッチあたりのデータ件数
+            test_batch_size = len(labels_test)
+            # 1エポックあたりのデータ累積件数
+            n_test += test_batch_size
+
+            # GPUヘ転送
+            inputs_test = inputs_test.to(device)
+            labels_test = labels_test.to(device)
+
+            # 予測計算
+            outputs_test = net(inputs_test)
+
+            # 損失計算
+            loss_test = criterion(outputs_test, labels_test)
+ 
+            # 予測ラベル導出
+            predicted_test = torch.max(outputs_test, 1)[1]
+
+            #  平均前の損失と正解数の計算
+            # lossは平均計算が行われているので平均前の損失に戻して加算
+            val_loss +=  loss_test.item() * test_batch_size
+            n_val_acc +=  (predicted_test == labels_test).sum().item()
+    
+        # 精度計算
+        train_acc = n_train_acc / n_train
+        val_acc = n_val_acc / n_test
+        # 損失計算
+        avg_train_loss = train_loss / n_train
+        avg_val_loss = val_loss / n_test
+        # 結果表示
+        print (f'Epoch [{(epoch+1)}/{num_epochs+base_epochs}], loss: {avg_train_loss:.5f} acc: {train_acc:.5f} val_loss: {avg_val_loss:.5f}, val_acc: {val_acc:.5f}')
+        # 記録
+        item = np.array([epoch+1, avg_train_loss, train_acc, avg_val_loss, val_acc])
+        history = np.vstack((history, item))
+    return history
+
+
+
+# 学習ログ解析
+def evaluate_history(history):
+  #損失と精度の確認
+  print(f'初期状態: 損失: {history[0,3]:.5f} 精度: {history[0,4]:.5f}') 
+  print(f'最終状態: 損失: {history[-1,3]:.5f} 精度: {history[-1,4]:.5f}' )
+
+  num_epochs = len(history)
+  if num_epochs < 10:
+    unit = 1
+  else:
+    unit = num_epochs / 10
+
+  # 学習曲線の表示 (損失)
+  plt.figure(figsize=(9,8))
+  plt.plot(history[:,0], history[:,1], 'b', label='訓練')
+  plt.plot(history[:,0], history[:,3], 'k', label='検証')
+  plt.xticks(np.arange(0,num_epochs+1, unit))
+  plt.xlabel('繰り返し回数')
+  plt.ylabel('損失')
+  plt.title('学習曲線(損失)')
+  plt.legend()
+  plt.show()
+
+  # 学習曲線の表示 (精度)
+  plt.figure(figsize=(9,8))
+  plt.plot(history[:,0], history[:,2], 'b', label='訓練')
+  plt.plot(history[:,0], history[:,4], 'k', label='検証')
+  plt.xticks(np.arange(0,num_epochs+1,unit))
+  plt.xlabel('繰り返し回数')
+  plt.ylabel('精度')
+  plt.title('学習曲線(精度)')
+  plt.legend()
+  plt.show()
+
+
+# イメージとラベル表示
+def show_images_labels(loader, classes, net, device):
+
+    # DataLoaderから最初の1セットを取得する
+    for images, labels in loader:
+        break
+    # 表示数は50個とバッチサイズのうち小さい方
+    n_size = min(len(images), 50)
+
+    if net is not None:
+      # デバイスの割り当て
+      inputs = images.to(device)
+      labels = labels.to(device)
+
+      # 予測計算
+      outputs = net(inputs)
+      predicted = torch.max(outputs,1)[1]
+      #images = images.to('cpu')
+
+    # 最初のn_size個の表示
+    plt.figure(figsize=(20, 15))
+    for i in range(n_size):
+        ax = plt.subplot(5, 10, i + 1)
+        label_name = classes[labels[i]]
+        # netがNoneでない場合は、予測結果もタイトルに表示する
+        if net is not None:
+          predicted_name = classes[predicted[i]]
+          # 正解かどうかで色分けをする
+          if label_name == predicted_name:
+            c = 'k'
+          else:
+            c = 'b'
+          ax.set_title(label_name + ':' + predicted_name, c=c, fontsize=20)
+        # netがNoneの場合は、正解ラベルのみ表示
+        else:
+          ax.set_title(label_name, fontsize=20)
+        # TensorをNumPyに変換
+        image_np = images[i].numpy().copy()
+        # 軸の順番変更 (channel, row, column) -> (row, column, channel)
+        img = np.transpose(image_np, (1, 2, 0))
+        # 値の範囲を[-1, 1] -> [0, 1]に戻す
+        img = (img + 1)/2
+        # 結果表示
+        plt.imshow(img)
+        ax.set_axis_off()
+    plt.show()
+
+
+# PyTorch乱数固定用
+
+def torch_seed(seed=123):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms = True
